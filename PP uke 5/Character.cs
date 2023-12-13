5540a38b0b07cb4d54125e0f64a9ed8e26d6c1e6
Viktor Stokstad <141615732+Vikkidub@users.noreply.github.com>
2023-12-13 09:55:53 +0100

    Add project files.

---
+++ PP uke 5/Character.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PP_uke_5
+{
+    internal class Character
+    {
+       public string Name { get; set; }
+       public string House { get; set; }
+       public string Inventory { get; set; }
+
+        public string wand { get; set; }
+        public string pet { get; set; }
+
+        public Character (string name, string house, string inventory)
+        {
+            Name = name;
+            House = house;
+            Inventory = inventory;
+        }
+    }
+}
---
+++ PP uke 5/Program.cs
@@ -0,0 +1,32 @@
+﻿using PP_uke_5;
+/* 
+ * Du skal starte med å lage en harrypotter character klasse med egenskaper som navn, house, inventory (ex wand eller pet)
+
+Få applikasjonen til å printe ut en introduksjon for karakteren, som sier noe om hva de heter, hvilket hus de er medlem av og hvilke items de har
+
+Karakteren skal ha mulighet til å gå inn i en Magibutikk, der kan de kjøpe et dyr:  ugle,rotte eller en katt. De har også mulighet til å kjøpe en tryllestav: føniksstav, unikornstav eller trollstav. For å få til dette må du lage en egen klasse som er butikken, og presentere brukeren med en meny for hva personen skal kjøpe.
+
+Karakteren skal ha mulighet til å trylle - ta inn input fra brukeren, og dersom en skriver en riktig trylleformel skal det printes til skjermen at de har utført tryllingen
+trylleformler: 
+vingardium leviosa (får en fjær til å fly)
+hokus pokus (fyrer av fyrverkerier)
+*/
+namespace PP_uke_5 {
+    internal class Program
+    {
+       
+        public string pet;
+        public string wand;
+        public List<Character> inventory { get; set; } = new List<Character>();
+
+        static void Main() { var character = new Character("","",""); }
+        void InventoryItems()
+        {
+            inventory = new List<Character>
+        {
+        new Character{pet = "ugle", wand = "enhjørningstav", inventory = "Griffindor" },
+        new Character{wand = "enhjørningstav" },
+        };
+        }
+    }
+}
